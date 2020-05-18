707ae1f16c0c931f813f3c4cef5f49466b5d0041
Krzysztof Bohdanowicz <kbohdanowicz@sigma.ug.edu.pl>
2020-05-18 21:29:30 +0200

    lab 12

---
+++ lab12/Chat/model/index.js
@@ -0,0 +1,65 @@
+const mongoose = require("../mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("../bcrypt");
+
+const messageSchema = new Schema({
+    handle: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    }
+});
+
+const chatRoomSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    messages: [messageSchema]
+});
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// bez poniższej wtyczki nie dostaniemy sensownego sygnału
+// błędu przy naruszeniu „unikatowości” nazwy użytkownika
+const uniqueValidator = require("mongoose-unique-validator");
+// ale z nią – już wszystko będzie jak należy
+userSchema.plugin(uniqueValidator);
+
+userSchema.methods.isValidPassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const Message = mongoose.model("Message", messageSchema);
+const ChatRoom = mongoose.model("ChatRoom", chatRoomSchema, "chatRooms");
+const User = mongoose.model("User", userSchema);
+
+const errorHandler = {};
+// mały „postprocessing” błędów mongoosowych
+errorHandler.processErrors = (err) => {
+    let msg = {};
+    for (let key in err.errors) {
+        msg[key] = err.errors[key].message;
+    }
+    return msg;
+};
+
+module.exports.message = Message;
+module.exports.chatRoom = ChatRoom;
+module.exports.user = User;
+module.exports.errorHandler = errorHandler;
