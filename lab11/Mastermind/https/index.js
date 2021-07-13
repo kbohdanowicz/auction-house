87f9780c548382edd430cf654900fe059bfb763b
Krzysztof Bohdanowicz <kbohdanowicz@sigma.ug.edu.pl>
2021-07-13 15:33:27 +0200

    Cleanup

--- lab11/Mastermind/https/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    const fs = require("fs");
-    const https = require("https");
-    return https.createServer({
-        key: fs.readFileSync("./https/my.key"),
-        cert: fs.readFileSync("./https/my.crt")
-    }, app);
-};
--- lab5/zad3.js
+++
@@ -1,8 +0,0 @@
-String.prototype.nbsp = function() {
-    return this.replace(/\s[a|i|o|u|w|z]\s/gm, function(word) {
-        return " " + word.trim() + "&nbsp;";
-    })
-};
-
-let tekst = 'Ala i As poszli w las';
-console.log(tekst.nbsp());
